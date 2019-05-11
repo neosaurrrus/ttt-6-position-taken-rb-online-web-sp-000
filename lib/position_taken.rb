52baf19e608a9889bb88e98a1ce95d823edde7d5
Lukie Kang <lukiek+f@gmail.com>
2019-05-11 22:14:54 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board,index)
+
+  if board[index] == "X"
+    true
+  elsif board[index] == "O"
+    true
+  else
+    false
+  end
+end
